378766e3cf2d8d27a03f607069ef464f995f46c7
LampKang <47997051+LampKang@users.noreply.github.com>
2020-05-21 16:52:13 +0900

    Update main.c

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main(){	
 	printf("hello world!\n");
-  printf("bye world!\n");
+	printf("bye world!\n");
 
-  printf("what???\n");
+	printf("what???\n");
 
-  printf("My name is Taeyoung-Han!\n");
+	printf("My name is Taeyoung-Han!\n");
 
-  for ( i=0 ; i< 5; i++)
-  {
-      printf("I hate COVID-19\n!");
-  }
-  printf("Linux class\n");
-  printf("Is very\n ");
-  printf("Nice class and \n");
-  printf("Utopia and e\n");
-  printf("Xiting!!!\n");
-  
-  printf("wow !!!\n");
-    
+	for ( i=0 ; i< 5; i++)
+	{
+	printf("I hate COVID-19\n!");
+	}
+	printf("Linux class\n");
+	printf("Is very\n ");
+	printf("Nice class and \n");
+	printf("Utopia and e\n");
+	printf("Xiting!!!\n");
+
+	printf("wow !!!\n");
+
 
 	return 1;
 
