ec94a412b0ccb567c90b6cca9fbfcea6903edd23
humun1 <eivy1115@daum.net>
2020-05-22 16:40:10 +0900

    add main.c and Modified

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -1,30 +1,8 @@
 #include <stdio.h>
 int main(){	
-	printf("hello world!\n");
-	printf("bye world!\n");
-
-	printf("what???\n");
-
-	printf("My name is Taeyoung-Han!\n");
-
-	for ( i=0 ; i< 5; i++)
-	{
-	printf("I hate COVID-19\n!");
-	}
-	printf("Linux class\n");
-	printf("Is very\n ");
-	printf("Nice class and \n");
-	printf("Utopia and e\n");
-	printf("Xiting!!!\n");
-
-	printf("wow !!!\n");
-
-	
-	printf("I write this in May");
-        
-        printf("Hi hi");
-
-
-	return 1;
-
+	printf("Hi!\n");
+	printf("2019203075\n");
+	printf("Modify this file.!\n");
+	printf("Good Bye!\n");
+	return 0;
 }
