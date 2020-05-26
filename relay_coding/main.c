b75d084483c30d1a4c1167baf605378aca74ef9a
linfengfei <linfengfei@naver.com>
2020-05-26 15:15:53 +0900

    add main.c

--- relay_coding/main.c
+++ relay_coding/main.c
@@ -1,49 +1,6 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hi!\n");
-	printf("2019203075\n");
-	printf("Modify this file.!\n");
-	printf("Good Bye!\n");
-
-	printf("hello world!\n");
-	printf("bye world!\n");
-
-	printf("what???\n");
-
-	printf("My name is Taeyoung-Han!\n");
-
-	for(int i = 0 ; i < 5; i++) {
-		printf("I hate COVID-19!\n");
-	}
-	printf("Linux class\n");
-	printf("Is very\n ");
-	printf("Nice class and \n");
-	printf("Utopia and e\n");
-	printf("Xiting!!!\n");
-
-	printf("wow !!!\n");
-
-	printf("I write this in May\n");
-
-	printf("Hi hi");
-
-	printf("So difficult ...\n");
-	printf("I want to meet new friends!\n");
-
-	printf("I hate COVID-19. too\n");
-	printf("i want go to school\n");
-
-	printf("I Love C\n");
-  
-	printf("The Cake is a Lie!\n")
-
-	printf("\ncivilization 6 is amazing game!\n");
-	printf("please fix bugs\n");
-
-	printf("Hello nice to meet you.\n");
-
-	printf("I Love C\n");
-
+	printf("my name is linfengfei."\n);
 	return 1;
 }
